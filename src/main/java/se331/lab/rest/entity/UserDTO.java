d924de73028da60fe79c2e4e7c47ef361d103922
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-10-21 16:54:50 +0700

    first commit

---
+++ src/main/java/se331/lab/rest/entity/UserDTO.java
@@ -0,0 +1,17 @@
+package se331.lab.rest.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    Long id;
+    String username;
+    String email;
+    String password;
+}
